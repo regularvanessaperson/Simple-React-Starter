6abacb6f580052ad3bcd88471860b2f28eb5c4fb
regularvanessaperson <vamavila4@gmail.com>
2020-11-27 15:55:20 -0600

    calculator works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 
-import FunctionalComponent from "./components/FunctionalComponent";
-import ClassComponent from "./components/ClassComponent";
+// import FunctionalComponent from "./components/FunctionalComponent";
+import Calculator from "./components/Calculator";
 
 import "./css/App.css";
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <FunctionalComponent />
-        <ClassComponent />
+        {/* <FunctionalComponent /> */}
+        <Calculator />
       </div>
     );
   }
